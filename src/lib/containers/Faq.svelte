87848c4e59d364ad056e6c7dbc9036afb8be9e34
Samuel Aditia <samuel.aditia@ruparupa.com>
2024-04-02 14:13:10 +0700

    feat: add faq container animation

--- src/lib/containers/Faq.svelte
+++ src/lib/containers/Faq.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import clsx from 'clsx';
+
 	const faqs = [
 		{
 			question: 'What’s the different between Premium Template and Customized Design?',
@@ -10,35 +12,46 @@
 			question: 'How long does a set up for Premium Template ?',
 			answer:
 				'Premium Template is Another Chapter ready responsive designs, you can look and select in our template collections for `your website invitation. Customized Design is your own personalized website invitation design that reflects your unique style and story.',
-			isOpen: true
+			isOpen: false
 		},
 		{
 			question: 'How long does a Customized Website Design take?',
 			answer:
 				'Premium Template is Another Chapter ready responsive designs, you can look and select in our template collections for `your website invitation. Customized Design is your own personalized website invitation design that reflects your unique style and story.',
-			isOpen: true
+			isOpen: false
 		},
 		{
 			question: 'What can I change from the premium template?',
 			answer:
 				'Premium Template is Another Chapter ready responsive designs, you can look and select in our template collections for `your website invitation. Customized Design is your own personalized website invitation design that reflects your unique style and story.',
-			isOpen: true
+			isOpen: false
 		}
 	];
+
+	const handleOpen = (index: number) => {
+		faqs[index].isOpen = !faqs[index].isOpen;
+	};
 </script>
 
 <div class="min-h-screen w-screen bg-ac-light-cream">
 	<div class="container py-20">
 		<div class="font-roman text-4xl font-light">Frequently Asked Questions</div>
 		<div class="mt-12 flex flex-col gap-6">
-			{#each faqs as faq}
+			{#each faqs as faq, index}
 				<div class="w-full flex flex-col gap-4">
 					<div class="w-full border-t border-solid border-ac-black" />
 					<div class="flex justify-between items-center w-full">
 						<div class="font-helvetica text-xl/normal">{faq.question}</div>
-						<div class="text-2xl font-light">+</div>
+						<button class="text-2xl font-light" on:click={() => handleOpen(index)}
+							>{faq.isOpen ? '-' : '+'}</button
+						>
 					</div>
-					<div class="font-helvetica">
+					<div
+						class={clsx(
+							'font-helvetica transform transition-all overflow-hidden max-h',
+							faq.isOpen ? 'max-h-80' : 'max-h-0'
+						)}
+					>
 						{faq.answer}
 					</div>
 				</div>
